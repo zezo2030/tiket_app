e9320c5db6db718a35c2e32fb4cd20fa9d8a7d44
zoz <zezosaad194@gmail.com>
2024-12-16 13:04:35 +0200

    update

---
+++ lib/base/utils/all_jason.dart
@@ -0,0 +1,124 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code': "NYC",
+      'name': "New-York",
+    },
+    'to': {
+      'code': "LDN",
+      'name': "London",
+    },
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    "number": 23
+  },
+  {
+    'from': {
+      'code': "DK",
+      'name': "Dhaka",
+    },
+    'to': {
+      'code': "SH",
+      'name': "Shanghai",
+    },
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time': "09:00 AM",
+    "number": 45
+  },
+  {
+    'from': {
+      'code': "LA",
+      'name': "Los Angeles",
+    },
+    'to': {
+      'code': "TKY",
+      'name': "Tokyo",
+    },
+    'flying_time': '11H 15M',
+    'date': "15 MAY",
+    'departure_time': "10:00 AM",
+    "number": 67
+  },
+  {
+    'from': {'code': "SYD", 'name': "Sydney"},
+    'to': {'code': "BKK", 'name': "Bangkok"},
+    'flying_time': '9H 45M',
+    'date': "20 MAY",
+    'departure_time': "07:00 AM",
+    "number": 89
+  },
+  {
+    'from': {'code': "PAR", 'name': "Paris"},
+    'to': {'code': "BER", 'name': "Berlin"},
+    'flying_time': '1H 50M',
+    'date': "25 MAY",
+    'departure_time': "06:00 AM",
+    "number": 12
+  },
+  {
+    'from': {'code': "MEX", 'name': "Mexico City"},
+    'to': {'code': "SFO", 'name': "San Francisco"},
+    'flying_time': '5H 30M',
+    'date': "30 MAY",
+    'departure_time': "11:00 AM",
+    "number": 34
+  },
+  {
+    'from': {'code': "ROM", 'name': "Rome"},
+    'to': {'code': "AMS", 'name': "Amsterdam"},
+    'flying_time': '2H 10M',
+    'date': "5 JUN",
+    'departure_time': "12:00 PM",
+    "number": 56
+  },
+  {
+    'from': {'code': "CAI", 'name': "Cairo"},
+    'to': {'code': "JNB", 'name': "Johannesburg"},
+    'flying_time': '8H 00M',
+    'date': "10 JUN",
+    'departure_time': "01:00 PM",
+    "number": 78
+  },
+  {
+    'from': {'code': "SIN", 'name': "Singapore"},
+    'to': {'code': "HKG", 'name': "Hong Kong"},
+    'flying_time': '3H 45M',
+    'date': "15 JUN",
+    'departure_time': "02:00 PM",
+    "number": 90
+  },
+  {
+    'from': {'code': "BOM", 'name': "Mumbai"},
+    'to': {'code': "DXB", 'name': "Dubai"},
+    'flying_time': '2H 50M',
+    'date': "20 JUN",
+    'departure_time': "03:00 PM",
+    "number": 21
+  },
+  {
+    'from': {'code': "IST", 'name': "Istanbul"},
+    'to': {'code': "ATH", 'name': "Athens"},
+    'flying_time': '1H 30M',
+    'date': "25 JUN",
+    'departure_time': "04:00 PM",
+    "number": 43
+  },
+  {
+    'from': {'code': "MEL", 'name': "Melbourne"},
+    'to': {'code': "AKL", 'name': "Auckland"},
+    'flying_time': '3H 30M',
+    'date': "30 JUN",
+    'departure_time': "05:00 PM",
+    "number": 65
+  },
+  {
+    'from': {'code': "LAX", 'name': "Los Angeles"},
+    'to': {'code': "SYD", 'name': "Sydney"},
+    'flying_time': '14H 50M',
+    'date': "5 JUL",
+    'departure_time': "06:00 PM",
+    "number": 87
+  }
+];
